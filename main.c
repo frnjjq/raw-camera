82c771d3a8fce3c40426d0d9cfbdcfce5af82e6e
frnjjq <frnjjq@gmail.com>
2018-11-12 18:20:21 +0100

    Added util and vector bayer transformation

--- main.c
+++ main.c
@@ -3,9 +3,11 @@
 
 #include "capture.h"
 #include "player.h"
+#include "util.c"
 
 void  raw_to_shitty_rgb(uint8_t *rgb_data, uint8_t *raw_data, int width, int height);
 void  raw_to_rgb(uint8_t *rgb_data, uint8_t *raw_data, int width, int height);
+void  raw_to_rgb_vec(uint8_t * restrict rgb_data, uint8_t * restrict raw_data, int width, int height);
 
 //int main( int argc, const char* argv[] )
 int main()
@@ -15,18 +17,20 @@ int main()
     bool quit;
     uint8_t *rgb_data;
 	camera = start_camera(filename);
-    rgb_data = (uint8_t *) malloc (camera.width * camera.height *3 );
+    rgb_data = (uint8_t *) malloc(camera.width * camera.height *3 );
     init_player(camera.width, camera.height);
     
     while(!quit)
     {
         handle_window(&quit);
         get_frame( camera);
+        //start_counting();
         raw_to_rgb(rgb_data, camera.buffer_start, camera.width, camera.height);
+        //stop_counting();
         play_frame((void *)rgb_data, camera.width*3);
     }
     close_player();
-    return close_camera(camera);;
+    return close_camera(camera);
 }
 
 void  raw_to_shitty_rgb(uint8_t *rgb_data, uint8_t *raw_data, int width, int height)
@@ -97,4 +101,73 @@ void  raw_to_rgb(uint8_t *rgb_data, uint8_t *raw_data, int width, int height)
             rgb_data[3*(y*width+x)+2] = b;
         }
     }
+}
+
+void  raw_to_rgb_vec(uint8_t * restrict rgb_data, uint8_t * restrict raw_data, int width, int height)
+{
+    uint8_t * restrict r_data_odd = aligned_alloc(64, height* width);
+    uint8_t * restrict g_data_odd = aligned_alloc(64, height* width);
+    uint8_t * restrict b_data_odd = aligned_alloc(64, height* width);
+
+    uint8_t * restrict r_data_even = aligned_alloc(64, height* width);
+    uint8_t * restrict g_data_even = aligned_alloc(64, height* width);
+    uint8_t * restrict b_data_even = aligned_alloc(64, height* width);
+
+    for (int y = 1; y< height-1; y+=2)
+    {
+        for (int x = 1; x< width; x++) // Valid for odd columns
+        {
+            r_data_odd[y*width+x] = raw_data[y*width+x];
+            g_data_odd[y*width+x] = (raw_data[(y-1)*width+x] + raw_data[(y+1)*width+x] + raw_data[y*width+x+1] +raw_data[y*width+x-1])/4;
+            b_data_odd[y*width+x] = (raw_data[(y-1)*width+x-1] + raw_data[(y+1)*width+x+1] + raw_data[(y-1)*width+x+1] +raw_data[(y+1)*width+x-1])/4;
+        }
+    }
+    for (int y = 2; y< height-1; y+=2)
+    {
+        for (int x = 1; x< width-1; x++) // Valid for odd columns
+        {
+            r_data_odd[y*width+x] = (raw_data[(y-1)*width+x] + raw_data[(y+1)*width+x])/2;
+            g_data_odd[y*width+x] = raw_data[y*width+x];
+            b_data_odd[y*width+x] = (raw_data[y*width+x+1] + raw_data[y*width+x-1])/2;
+        }
+    }
+    for (int y = 1; y< height-1; y+=2)
+    {
+        for (int x = 2; x< width-1; x++) //Even column
+        {
+            r_data_even[y*width+x] = (raw_data[(y-1)*width+x] + raw_data[(y+1)*width+x])/2;
+            g_data_even[y*width+x] = raw_data[y*width+x];
+            b_data_even[y*width+x] = (raw_data[y*width+x+1] + raw_data[y*width+x-1])/2;
+        }
+    }
+    for (int y = 2; y< height-1; y+=2)
+    {
+        for (int x = 2; x< width-1; x++) //Even column and even row
+        {
+            r_data_even[y*width+x] = (raw_data[(y-1)*width+x-1] + raw_data[(y+1)*width+x+1] + raw_data[(y-1)*width+x+1] +raw_data[(y+1)*width+x-1])/4;
+            g_data_even[y*width+x] = (raw_data[(y-1)*width+x] + raw_data[(y+1)*width+x] + raw_data[y*width+x+1] +raw_data[y*width+x-1])/4;
+            b_data_even[y*width+x] = raw_data[y*width+x];
+        }
+    }
+    for (int x = 0; x< width-1; x++)
+    {
+        for (int y = 0; y< height-1; y+=2)
+        {
+            rgb_data[3*(y*width+x)] = r_data_even[y*width+x];
+            rgb_data[3*(y*width+x)+1] = g_data_even[y*width+x];
+            rgb_data[3*(y*width+x)+2] = b_data_even[y*width+x];
+        }
+        for (int y = 1; y< height-1; y+=2)
+        {
+            rgb_data[3*(y*width+x)] = r_data_odd[y*width+x];
+            rgb_data[3*(y*width+x)+1] = g_data_odd[y*width+x];
+            rgb_data[3*(y*width+x)+2] = b_data_odd[y*width+x];
+        }
+    }
+    free(r_data_odd);
+    free(g_data_odd);
+    free(b_data_odd);
+    free(r_data_even);
+    free(g_data_even);
+    free(b_data_even);
 }
---
+++ util.c
@@ -0,0 +1,23 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+inline void start_counting();
+inline void stop_counting();
+struct timeval start, end;
+
+inline void start_counting()
+{
+    gettimeofday(&start, NULL);
+    return;
+}
+
+inline void stop_counting()
+{
+    int deltaInUSecs;
+    gettimeofday(&end, NULL);
+
+    deltaInUSecs = (end.tv_sec - start.tv_sec)*1000000 - (end.tv_usec - start.tv_usec);
+    printf("delta in microseconds\n %u\n\n", deltaInUSecs);
+    return;
+}
